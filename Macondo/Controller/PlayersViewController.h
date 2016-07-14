b754b86d1edcc179e20e992eacb3901df0bdc40e
wangchangyang <1258866830@qq.com>
2016-07-14 15:49:26 +0800

    fix
    fix

---
+++ Macondo/Controller/PlayersViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayersViewController.h
+//  Macondo
+//
+//  Created by wangchangyang on 16/7/14.
+//  Copyright © 2016年 wangchangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayersViewController : UITableViewController
+
+@end
---
+++ Macondo/Controller/PlayersViewController.m
@@ -0,0 +1,13 @@
+//
+//  PlayersViewController.m
+//  Macondo
+//
+//  Created by wangchangyang on 16/7/14.
+//  Copyright © 2016年 wangchangyang. All rights reserved.
+//
+
+#import "PlayersViewController.h"
+
+@implementation PlayersViewController
+
+@end
