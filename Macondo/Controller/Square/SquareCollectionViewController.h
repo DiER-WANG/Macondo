fa1327e05a94ac7ea18fd81dc1643a549797b3d3
wangchangyang <1258866830@qq.com>
2016-07-22 18:56:16 +0800

    add collectionVC

---
+++ Macondo/Controller/Square/SquareCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  SquareCollectionViewController.h
+//  Macondo
+//
+//  Created by wangchangyang on 16/7/22.
+//  Copyright © 2016年 wangchangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SquareCollectionViewController : UICollectionViewController
+
+@end
---
+++ Macondo/Controller/Square/SquareCollectionViewController.m
@@ -0,0 +1,93 @@
+//
+//  SquareCollectionViewController.m
+//  Macondo
+//
+//  Created by wangchangyang on 16/7/22.
+//  Copyright © 2016年 wangchangyang. All rights reserved.
+//
+
+#import "SquareCollectionViewController.h"
+
+@interface SquareCollectionViewController ()
+
+@end
+
+@implementation SquareCollectionViewController
+
+static NSString * const reuseIdentifier = @"AlbumCell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //UIEdgeInsets sectionInset = UIEdgeInsetsMake(50, 20, 50, 20);
+    
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark <UICollectionViewDataSource>
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+
+    return 0;
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+
+    return 10;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+/*
+// Uncomment this method to specify if the specified item should be highlighted during tracking
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
+}
+*/
+
+/*
+// Uncomment this method to specify if the specified item should be selected
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+*/
+
+/*
+// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	return NO;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	
+}
+*/
+
+@end
